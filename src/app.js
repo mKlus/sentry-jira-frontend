f237db6e25f909e9065529787c1ef33d313a40ad
Marek Klus <mklus@atlassian.com>
2022-03-17 15:17:17 +1100

    sentry test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://1f22f969a9fa440eba239131406bbc98@o1169785.ingest.sentry.io/6262987",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 console.log('Hello world!');
+
+myUndefinedFunction();
